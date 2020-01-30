45636c89723dfd17b9da86b2705fd62b9866e4f9
Benjamin Small <ben20262@aim.com>
2020-01-30 15:56:34 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,7 +1,14 @@
 class Actor < ActiveRecord::Base
   has_many :characters
-  has_many :shows
+  has_many :shows through: :characters
 
   def full_name
     self.first_name + self.last_name
+  end
+
+  def list_roles
+    self.chracters do |role|
+      role
+    end
+  end
 end
